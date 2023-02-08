5762ffc76724b74222cd691015b65d312cc080ea
othsueh <ych930719@gmail.com>
2023-02-08 11:01:04 +0800

    finish project 2

--- source/JSbasic/complete-javascript-course/06-Modal/starter/script.js
+++ source/JSbasic/complete-javascript-course/06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+const call = message => document.querySelector(message);
+const callAll = message => document.querySelectorAll(message);
+const remove = message => message.classList.remove('hidden');
+const add = message => message.classList.add('hidden');
+const modal = call(".modal");
+const overlay = call('.overlay');
+const close = call('.close-modal');
+const open = callAll('.show-modal');
+const addHidden = function(){
+    add(modal);
+    add(overlay); 
+};
+const removeHidden = function(){
+    console.log(`Button clicked`);  
+    remove(modal);
+    remove(overlay);
+};
+for(let i = 0 ; i < open.length; i++)
+    open[i].addEventListener('click',removeHidden);
+
+close.addEventListener('click',addHidden);
+overlay.addEventListener('click',addHidden);
+document.addEventListener('keydown',function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){ 
+        addHidden();
+    }
+});
