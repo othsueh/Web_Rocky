965ee08dde9f3082d6c5f3424598bc637869bc26
othsueh <ych930719@gmail.com>
2023-04-27 15:46:33 +0800

    review js

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -1,59 +1,104 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-const flightList = flights.split('+');
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flightList = flights.split('+');
 
-for(const f of flightList){
-  const components = f.split(';');
-  const final = [];
-  if(components[0].startsWith('_Delay')) final.push('🚨');
-  final.push(`${components[0].replaceAll('_',' ')} from ${components[1].slice(0,3).toUpperCase()} to ${components[2].slice(0,3).toUpperCase()} (${components[3].replace(':','h')})`);
-  console.log(final.join(''));
-}
-// Data needed for first part of the section
-const weekDays = ['mon','tue','wed','thu','fri','sat','sun'];
-const openingHours= {
-    [weekDays[3]]: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  };
+// for(const f of flightList){
+//   const components = f.split(';');
+//   const final = [];
+//   if(components[0].startsWith('_Delay')) final.push('🚨');
+//   final.push(`${components[0].replaceAll('_',' ')} from ${components[1].slice(0,3).toUpperCase()} to ${components[2].slice(0,3).toUpperCase()} (${components[3].replace(':','h')})`);
+//   console.log(final.join(''));
+// }
+// // Data needed for first part of the section
+// const weekDays = ['mon','tue','wed','thu','fri','sat','sun'];
+// const openingHours= {
+//     [weekDays[3]]: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   };
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  getMenu: function(starter, main){
-    return [this.starterMenu[starter],this.mainMenu[main]];
-  },
-  //compare with A & B expression
-  openingHours, 
-  //A
-  orderDelivery(starterIndex = 0, mainIndex = 0, address = "Where", time = "21:00"){
-    console.log(`I receive your order, ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`);
-  },
-  //B 
-  orderPasta: function(ing1, ing2, ing3){
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`)
-  },
-  orderPizza: function(mainFlavor, ...otherFlavors){
-    console.log(`The main flavor of your pizza is ${mainFlavor}.`);
-    console.log(`Ohter is/are ${otherFlavors}.`);
-  }
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   getMenu: function(starter, main){
+//     return [this.starterMenu[starter],this.mainMenu[main]];
+//   },
+//   //compare with A & B expression
+//   openingHours, 
+//   //A
+//   orderDelivery(starterIndex = 0, mainIndex = 0, address = "Where", time = "21:00"){
+//     console.log(`I receive your order, ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`);
+//   },
+//   //B 
+//   orderPasta: function(ing1, ing2, ing3){
+//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`)
+//   },
+//   orderPizza: function(mainFlavor, ...otherFlavors){
+//     console.log(`The main flavor of your pizza is ${mainFlavor}.`);
+//     console.log(`Ohter is/are ${otherFlavors}.`);
+//   }
+// };
+const game = {
+team1: 'Bayern Munich',
+team2: 'Borrussia Dortmund',
+players: [
+[
+'Neuer',
+'Pavard',
+'Martinez',
+'Alaba',
+'Davies',
+'Kimmich',
+'Goretzka',
+'Coman',
+'Muller',
+'Gnarby',
+'Lewandowski',
+],
+[
+'Burki',
+'Schulz',
+'Hummels',
+'Akanji',
+'Hakimi',
+'Weigl',
+'Witsel',
+'Hazard',
+'Brandt',
+'Sancho',
+'Gotze',
+],
+],
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+'Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+team1: 1.33,
+x: 3.25,
+team2: 6.5,
+},
 };
-
+const [players1,players2] = game.players;
+const [gk,...fieldPlayers]= players1;
+const allPlayers = [...players1,...players2];
+const team1Substitute = ['Thiago', 'Coutinho','Perisic'];
+const players1Final = [...players1,...team1Substitute];
 // document.body.append(document.createElement('textarea'));
 // document.body.append(document.createElement('button'));
 
