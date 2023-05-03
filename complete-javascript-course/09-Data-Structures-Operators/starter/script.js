af3fe32ff95234f1b2199d0c72e6b367c935f955
othsueh <ych930719@gmail.com>
2023-05-03 11:31:03 +0800

    finish learning call and apply method

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 // // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flightList = flights.split('+');
+console.log(flightList);
+for(const message of flightList){
+    const [status,from,to,time] = message.split(';');
+    console.log(`${status.startsWith('_Delayed') ? '🚨': ''} ${status.replaceAll('_',' ').trim()} from ${from.slice(0,3).toUpperCase()} to ${to.slice(0,3).toUpperCase()} (${time.replace(':', 'h')})`);
+}
 // const flightList = flights.split('+');
 
 // for(const f of flightList){
@@ -180,19 +186,19 @@
 // console.log('   hello world   '.trim());
 // const threeDoor = "door door door";
 // console.log(threeDoor.replace(/door/g,'gate'));
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-document.querySelector('button').addEventListener('click',function(){
-    const text = document.querySelector('textarea').value;
-    const rows = text.split('\n');
-    console.log(rows);
-    for(const [i,str] of rows.entries()){
-        let [first,second] = str.toLowerCase().trim().split('_');
-        const output = `${first}${second.replace(second[0],second[0].toUpperCase())}`
-        console.log(`${output.padEnd(20)}${'✅'.repeat(i+1)}`);
-    }
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+// document.querySelector('button').addEventListener('click',function(){
+//     const text = document.querySelector('textarea').value;
+//     const rows = text.split('\n');
+//     console.log(rows);
+//     for(const [i,str] of rows.entries()){
+//         let [first,second] = str.toLowerCase().trim().split('_');
+//         const output = `${first}${second.replace(second[0],second[0].toUpperCase())}`
+//         console.log(`${output.padEnd(20)}${'✅'.repeat(i+1)}`);
+//     }
     
-})
+// })
 // const camelCaseTrans = (str)=>{
 //     let [first,second] = str.toLowerCase().trim().split('_');
 //     const output = `${first}${second.replace(second[0],second[0].toUpperCase())}`
--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+// topic: Higher Order Function and CallBack Function
+// const oneWord = (str) =>{
+//     return str.replace(/\s/g, '').toLowerCase();
+// }
+// const upperFirstWord = (str) =>{
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// }
+
+// const transformer = (str, fn) =>{
+//     console.log(`Original String: ${str}`);
+//     console.log(`Transformed String: ${fn(str)}`);
+//     console.log(`Transformed by: ${fn.name}`);
+// }
+// const strr = 'Hello World! Nice to Meet you'
+// transformer(strr, oneWord);
+// const high5 = () =>{
+//     console.log(`👋`);
+// }
+// document.body.addEventListener('click',high5)
+// const greeter = ['Jonas', 'Martha', 'hello'];
+// greeter.forEach(high5);
+
+
+// topic : Functions Returning Functions
+// const greet = (greeting)=>{
+//     return function(name){
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
+// greet('Hey')('Jonas');
+
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+// greetArr('Hello')('Jonas');
+
+// TOPIC : The call and apply Methods
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     //book: function(){}
+//     book(flightNum, name){
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+//         this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});
+//     }
+// }
+// lufthansa.book(239, 'Jonas Schmedtmann');
+// lufthansa.book(245, 'Adan Smith');
+
+// subtopic : Call Method
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: [],
+// };
+// const book = lufthansa.book;
+
+// //!Not Worked
+// // book(23, 'Sarah Williams');
+// book.call(lufthansa, 23, 'Sarah Williams');
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(lufthansa.bookings);
+// console.log(eurowings.bookings);
+
+//subtopic : Apply Method
+// const flightData = [583, 'George Cooper'];
+// book.apply(eurowings,flightData);
+// // *same
+// book.call(eurowings, ...flightData);
