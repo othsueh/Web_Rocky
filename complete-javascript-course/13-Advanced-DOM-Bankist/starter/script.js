9a5cb643fca26ccc64ecb4040cf1c62a63299e0c
othsueh <ych930719@gmail.com>
2023-07-17 20:52:32 +0800

    learning basic of DOM

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn=>btn.addEventListener('click',openModal))
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,35 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//topic : selecting, creating and deleting elements
+//subtopic : selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+document.getElementById('section--1');
+const allButtons2 = document.querySelectorAll('button');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(allButtons === allButtons2);
+console.log(document.getElementsByClassName('btn'));
+
+//subtopic : creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+//subtopic : deleting elements
+document.querySelector('.btn--close-cookie').
+addEventListener('click',function(){
+  message.remove();//new method
+  // message.parentElement.removeChild(message);//old method
+})
