b5fdd97932fbcdd8532f752c04abd8ff31a0568c
othsueh <ych930719@gmail.com>
2023-08-03 20:45:05 +0800

    finish coding challenge #3

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -1,28 +1,59 @@
 'use strict';
 
+//topic : coding challenge #3
+// const Car = function(make,speed){
+//     this.make = make;
+//     this.speed = speed;
+// }
+// Car.prototype.accelerate = function(){
+//     this.speed+=10;
+//     console.log(this.speed);
+// }
+// Car.prototype.brake = function(){
+//     this.speed-=5;
+//     console.log(this.speed);
+// }
+// const EV = function(make,speed,charge){
+//     Car.call(this,make,speed);
+//     this.charge = charge;
+// }
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.constructor = EV;
+// EV.prototype.chargeBattery = function(chargeTo){
+//     this.charge = chargeTo;
+// }
+// EV.prototype.accelerate = function(){
+//     this.speed+=20;
+//     this.charge--;
+//     console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+// }
+// const tesla = new EV('Tesla',120,23);
+// tesla.chargeBattery(90);
+// tesla.accelerate();
+
 //topic : inheritance between classes
 //subtopic : parent class
-const Person = function(firstName, year){
-    this.firstName = firstName;
-    this.year = year;
-}
+// const Person = function(firstName, year){
+//     this.firstName = firstName;
+//     this.year = year;
+// }
 //subtopic : child class
-const student = function(firstName, year, course){
-    Person.call(this,firstName, year);
-    this.course = course;
-};
+// const student = function(firstName, year, course){
+//     Person.call(this,firstName, year);
+//     this.course = course;
+// };
 // subtopic : linking prototype
-student.prototype = Object.create(Person.prototype);
-student.prototype.constructor = student;
-student.prototype.introduce = function(){
-    console.log(`My name is ${this.firstName} and I study ${this.course}`);
-}
+// student.prototype = Object.create(Person.prototype);
+// student.prototype.constructor = student;
+// student.prototype.introduce = function(){
+//     console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// }
 //subtopic : instance
-const mike = new student('Mike',2020,'Computer Science');
-mike.introduce();
-console.log(mike.__proto__);
-console.log(mike instanceof student);
-console.log(mike instanceof Person);
+// const mike = new student('Mike',2020,'Computer Science');
+// mike.introduce();
+// console.log(mike.__proto__);
+// console.log(mike instanceof student);
+// console.log(mike instanceof Person);
 //topic : coding challenge #2
 // class CarCl{
 //     constructor(make,speed){
