8215f4617199ffddc0946c8113a5c0dc0bf56ba7
othsueh <ych930719@gmail.com>
2023-08-11 22:07:45 +0800

    finish half of geolocation app

--- complete-javascript-course/15-Mapty/starter/script.js
+++ complete-javascript-course/15-Mapty/starter/script.js
@@ -11,13 +11,56 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
+//topic : Refactoring for Project Architecture
+class App{
+    #map;
+    #mapEvent;
+    constructor(){
+        this._getPosition();
+        form.addEventListener('submit',this._newWorkout.bind(this));
+        inputType.addEventListener('change',this._toggleElevationField);
+    }
+    _getPosition(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),function(){
+            alert('Could not get your position');
+        });
+    }
+
+    }
+    _loadMap(position){
         const {latitude} = position.coords;
         const {longitude} = position.coords;
-        console.log(`https://www.google.com.tw/maps/@${latitude},${longitude},7z?entry=ttu`);
-    },function(){
-        alert('Could not get your position');
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, 13);
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+        this.#map.on('click',this._showForm.bind(this));
     }
-    );
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+    _newWorkout(e){
+        e.preventDefault();
+        //subtopic : clear input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+        //subtopic : Display marker
+        const {lat, lng} = this.#mapEvent.latlng;
+        const newCoords = [lat, lng];
+        L.marker(newCoords).addTo(this.#map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        })).setPopupContent('Workout').openPopup();
+    }
 }
+const app = new App();
